14d884de66128947a1180cc724bd7760ea92f88a
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-11-07 12:55:26 +0530

    trip: Added endpoints for trip/

--- backend/functions/src/routes/trip.ts
+++ backend/functions/src/routes/trip.ts
@@ -1,11 +1,27 @@
 import { Router } from "express";
-import {getTrips, createTrip} from "../controllers/trip"
+import {forbidden, getAllTrips, getTrip, updateTrip, createTrip, deleteTrip, getUserTrips, deleteUserTrips, isCreator} from "../controllers/trip"
 
 const router = Router();
 
 router
   .route("/")
-  .get(getTrips)
-  .post(createTrip);
+  .get(getAllTrips)
+  .put(forbidden)
+  .post(forbidden)
+  .delete(forbidden);
+
+router
+  .route("/:id")
+  .get(getTrip)
+  .put(isCreator, updateTrip)
+  .post(forbidden)
+  .delete(isCreator, deleteTrip);
+
+  router
+  .route("/usr/:userId")
+  .get(getUserTrips)
+  .put(forbidden)
+  .post(createTrip)
+  .delete(deleteUserTrips);
 
 export default router;
