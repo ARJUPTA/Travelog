c4dab591a754cbf6a5007b62a39fa9a07ff933d2
Lakshya Singh <lakshay.singh1108@gmail.com>
2020-11-08 00:08:19 +0530

    chore: use iso string instead of normal

--- backend/functions/src/utils/airline.ts
+++ backend/functions/src/utils/airline.ts
@@ -7,8 +7,22 @@ export const BaseUrl = `https://www.cleartrip.com/node/flight/search?sd=16047458
 // const to = "DEL"
 // const sellingCountry = "IN"
 
-export const getFlightData = async ({depart_date, from, to, refCode}:{depart_date:string, from:string, to: string, refCode:string}): Promise<object> => {
-  const url = `${BaseUrl}&depart_date=${depart_date}&from=${from}&to=${to}&sellingCountry=IN&intl=n`
+export const getFlightData = async ({ depart_date, from, to, refCode }: { depart_date: string, from: string, to: string, refCode: string }): Promise<object> => {
+  let date:String|Date = new Date(depart_date);
+  const year = date.getFullYear();
+  let month:String|Number = date.getMonth()+1;
+  let dt:String|Number = date.getDate();
+
+  if (dt < 10) {
+    dt = '0' + dt;
+  }
+  if (month < 10) {
+    month = '0' + month;
+  }
+
+  date = `${dt}/${month}/${year}`
+
+  const url = `${BaseUrl}&depart_date=${date}&from=${from}&to=${to}&sellingCountry=IN&intl=n`
   const response: any = await axios.get(url);
   // console.log(response.data.cards[0])
 
