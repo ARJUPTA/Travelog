18865e005e8dca1beb49d29b3fc696ce4f7748f3
nb9960 <nbodani8@gmail.com>
2020-11-07 19:15:17 +0530

    auth changes

--- travelog/lib/providers/fireauth.dart
+++ travelog/lib/providers/fireauth.dart
@@ -1,42 +1,61 @@
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FireAuth {
-  static final FirebaseAuth _auth = FirebaseAuth.instance;
-  static final GoogleSignIn googleSignIn = GoogleSignIn();
+  auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
+  GoogleSignIn googleSignIn = GoogleSignIn();
 
-  static Future<String> signInWithGoogle() async {
-    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication =
+  Future<String> signInWithGoogle() async {
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
         await googleSignInAccount.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.credential(
+    auth.AuthCredential credential = auth.GoogleAuthProvider.credential(
       accessToken: googleSignInAuthentication.accessToken,
       idToken: googleSignInAuthentication.idToken,
     );
 
-    final UserCredential authResult =
+    auth.UserCredential authResult =
         await _auth.signInWithCredential(credential);
-    final User user = authResult.user;
+    auth.User user = authResult.user;
 
     if (user != null) {
       assert(!user.isAnonymous);
       assert(await user.getIdToken() != null);
 
-      final User currentUser = _auth.currentUser;
+      auth.User currentUser = _auth.currentUser;
       assert(user.uid == currentUser.uid);
 
       print('signInWithGoogle succeeded: $user');
 
-      return '$user';
+      var preferences = await SharedPreferences.getInstance();
+      preferences.setString("token", await user.getIdToken());
+
+      return '${user.displayName}';
     }
 
     return null;
   }
 
-  static Future<void> signOutGoogle() async {
+  Future<void> signOutGoogle() async {
     await googleSignIn.signOut();
 
     print("User Signed Out");
+  }
+
+  Future<bool> isLoggedIn() async {
+    var preferences = await SharedPreferences.getInstance();
+    if (preferences.containsKey("token")) {
+      auth.UserCredential authResult =
+          await _auth.signInWithCustomToken(preferences.get("token"));
+      auth.User user = authResult.user;
+      return user != null;
+    }
+    return false;
+  }
+
+  Future<String> signInWithEmail(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 }
